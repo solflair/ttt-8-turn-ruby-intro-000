0acdf4ed5128f98701cd77a07f0789ad11f8d63e
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-06 01:07:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,7 +20,7 @@ def input_to_index
 end
 
 def valid_move?(board, index)
-  if position_taken?(board, index) || !spot_exists?(board, index)
+  if position_taken?(board, index)
     return false
   else
     return true
@@ -30,19 +30,9 @@ end
 
 
 def position_taken?(board, index)
-  if board[index] == "X" || board[index] == "O"
-    return true
-
-  elsif board[index] == " " || board[index] == ""
-    return false
-  end
-end
-
-
-def spot_exists?(board, index)
-  if !(board[index] == nil)
-    return true
-  else
-    return false
-  end
+   if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+      return false 
+   else
+      return true
+   end
 end
