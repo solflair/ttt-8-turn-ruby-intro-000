7a6bb437812a2e55f377ffee2b797a94fc4fa2ca
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-06 01:13:41 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,9 +19,6 @@ def input_to_index(user_input)
    user_input.to_i - 1
 end
 
-def move(board, index, player)
-   board[index] = player
-end
 
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
