4803340afd24b4a4e63b56710a3b4b8f216e75de
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-06 02:00:08 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -39,9 +39,8 @@ def turn(board)
   index = input_to_index(user_input)
   if valid_move?(board, index)
     move(board, index, "X")
+    display_board(board)
   else
     turn(board)
  end
 end
-
-display_board(board)
