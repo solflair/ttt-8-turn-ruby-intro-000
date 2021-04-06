d44a92d3ab136ffadd299b9ea9721ddd7539bc9e
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-06 00:39:32 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -9,4 +9,33 @@ end
 
 def turn(board)
   puts "Please enter 1-9:"
+  gets user_input
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) || !spot_exists?(board, index)
+    return false
+  else
+    return true
+  end
+
+end
+
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+
+  elsif board[index] == " " || board[index] == ""
+    return false
+  end
+end
+
+
+def spot_exists?(board, index)
+  if !(board[index] == nil)
+    return true
+  else
+    return false
+  end
 end
