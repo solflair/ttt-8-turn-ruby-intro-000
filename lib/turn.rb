aaeb2c30a2f06bf99000258ce9ec3bd3732dbf27
Jasmine Alexander <solflair.jewelry@gmail.com>
2021-04-06 01:07:11 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -31,7 +31,7 @@ end
 
 def position_taken?(board, index)
    if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
-      return false 
+      return false
    else
       return true
    end
